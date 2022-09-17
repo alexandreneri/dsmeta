a5f289551aa620cd1e0ae9956f566d4519d34383
alexandre <alexandre.romulo@hotmail.com>
2022-09-17 18:43:35 -0300

    back end

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dsmeta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.service.SaleService;
+import com.devsuperior.dsmeta.service.SmsService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
+	
+
+//	@GetMapping
+//	public List<Sale> findSales() {
+//		return service.findSales();		
+//	}
+	
+//	@GetMapping
+//	public Page<Sale> findSales(Pageable pageable) {
+//		return service.findSales(pageable);		
+//	}
+	
+	@GetMapping
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);		
+	}
+//
+//	@GetMapping("/notification")
+//	public void notifySms() {
+//		smsService.sendSms();		
+//	}
+
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);		
+	}
+
+}
